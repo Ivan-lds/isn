3f5c8df9b47ccc9596de6b590dc7d43a69716613
Ivan Lima <ivanlimadossantos4@gmail.com>
2025-03-14 20:14:25 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // Caminho para o seu db.json
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
+server.use(router);
 
 server.listen(3000, () => {
-  console.log("JSON Server está rodando na porta 3000");
+  console.log("JSON Server is running");
 });
-
-module.exports = server;
